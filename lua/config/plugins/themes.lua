7f3632d427a613e5d823e88dbfb7ce930f425cdf
adamszegedi <mail@adamszegedi.eu>
2025-11-12 17:10:16 +0100

    Catppuccin colorscheme added

--- lua/config/plugins/themes.lua
+++ lua/config/plugins/themes.lua
@@ -1,5 +1,6 @@
 return {
     { 'rebelot/kanagawa.nvim', priority = 1000, opts = {}, },
     { 'folke/tokyonight.nvim',   lazy = false, priority = 1000, opts = {}, },
-    { 'ellisonleao/gruvbox.nvim', priority = 1000 , config = true, opts = ...}
+    { 'ellisonleao/gruvbox.nvim', priority = 1000 , config = true, opts = ...},
+    { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 }
 }
